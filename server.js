391ab587683b4ff2ff582f05a63f129b997b9563
kgreen14 <kgreen14@gmail.com>
2017-08-09 11:52:29 -0400

    added db and models directories and added schema, seeds and models

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 require("dotenv").config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 
 mongoose.Promise = global.Promise;
 
 // Mongoose Connection
 mongoose.connect(process.env.MONGODB_URI);
-
 const connection = mongoose.connection;
-connection.on('connected', () => {
-  console.log('Mongoose Connected Successfully');    
-}); 
 
-// If the connection throws an error
-connection.on('error', (err) => {  
-  console.log('Mongoose default connection error: ' + err);
-}); 
+connection.on('connected', () => {
+  console.log("Mongoose Connected Successfully");
+})
+
+connection.on('error', (err) => {
+  console.log("Mongoose connection error: " + err);
+})
 
 app.use(bodyParser.json());
-app.get('/', (req,res) => {
-  res.send('Hello world!')
-})
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log("Magic happening on port " + PORT);
-})
+  console.log("App is listening on: " + PORT);
+});

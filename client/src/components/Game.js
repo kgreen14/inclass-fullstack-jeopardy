b4d9e1e9ae8ddac188a5113f1acaa7f852e7f237
kgreen14 <kgreen14@gmail.com>
2017-08-09 14:51:57 -0400

    set the state in game.js and created componentWillMount, rendered the info from the state

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 
 class Game extends Component {
+    constructor() {
+        super();
+        this.state = {
+            id: "",
+            user: "",
+            points: "",
+            categories: "",
+            board: []
+        }
+    }
+
+    componentWillMount() {
+        const id = this.props.match.params.gameID
+        axios.get(`/api/game/${id}`).then((res) => {
+          this.setState({
+            id: res.data._id,
+            user: res.data.user,
+            points: res.data.points,
+            categories: res.data.categories,
+            board: res.data.board
+            })
+        })
+    }
 
     render() {
         return (
             <div>
-                Hello From Game
-                {this.props.match.params.gameID}
+                <h1>This Is Jeopardy!</h1>
+        <h3>Welcome {this.state.user}</h3>
+        <h3>Points: {this.state.points}</h3>
+        {/* <GameBoard board={this.state.board} categories={this.state.categories} /> */}
             </div>
         );
     }
